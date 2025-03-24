799149fe6eb3945d79005c681c78ebbe6a137b84
ikabeee <carlglz30@gmail.com>
2025-03-23 20:39:12 -0500

    feat: add Sensor interface and enable CORS in the application

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
 
 const app = express();
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(cors())
 
 dotenv.config();
 const PORT = process.env.PORT || 3000;
