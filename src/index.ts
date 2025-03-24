9923576aa5e6eb1bd07c94e6688dc4dd177a17ef
ikabeee <carlglz30@gmail.com>
2025-03-24 00:06:57 -0500

    feat: add sensor and plot data insertion intervals in server initialization

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import dotenv from 'dotenv';
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import { insertDataSensors } from './common/scripts/sensorApi';
+import { havePlotBeenDeleted, insertPlotData, insertPlotSensorData } from './common/scripts/plotApi';
 
 const app = express();
 app.use(express.json());
@@ -13,4 +15,16 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port: ${PORT}`);
+    setInterval(async () => {
+        await insertDataSensors();
+    }, 1000);
+    setInterval(async()=>{
+        await havePlotBeenDeleted();
+    }, 1000)
+    setInterval(async()=>{
+        await insertPlotData()
+    }, 1000)
+    setInterval(async()=>{
+        await insertPlotSensorData()
+    }, 1000)
 });
